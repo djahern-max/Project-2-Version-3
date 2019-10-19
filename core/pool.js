9b2db18c771cbb4717bb59e1e6d7eba8f796b8e3
Dano <danielaherniv@gmail.com>
2019-10-19 12:20:07 -0400

    changed connection

--- core/pool.js
+++ core/pool.js
@@ -1,24 +1,40 @@
 const util = require('util');
 const mysql = require('mysql');
 require("dotenv").config();
+let connection;
 
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'localhost',
-    user: process.env.USER_NAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-pool.getConnection((err, connection) => {
-    if (err)
-        console.error("Something went wrong connecting to the database ...");
+    // const pool = mysql.createPool({
+    //     connectionLimit: 10,
+    //     host: 'localhost',
+    //     user: process.env.USER_NAME,
+    //     password: process.env.PASSWORD,
+    //     database: process.env.DATABASE
+    // });
 
-    if (connection)
-        connection.release();
-    return;
-});
+    // pool.getConnection((err, connection) => {
+    //     if (err)
+    //         console.error("Something went wrong connecting to the database ...");
 
-pool.query = util.promisify(pool.query);
+    //     if (connection)
+    //         connection.release();
+    //     return;
+    // });
 
-module.exports = pool;
+    // pool.query = util.promisify(pool.query);
+
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: process.env.USER_NAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+    });
+}
+connection.connect();
+
+
+module.exports = connection;
